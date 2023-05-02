6e429d204130a6b5146ffd4da903bd00ef248521
c-c-k <c-c-k@nym.hush.com>
2023-05-02 12:10:51 +0300

    stages 1 & 2 done

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# don't change the following way to run flask:
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_host, arg_port = sys.argv[1].split(':')
+        app.run(host=arg_host, port=arg_port)
+    else:
+        app.run()
